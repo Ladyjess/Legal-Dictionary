ab438353d2586478ab47eb2362c1b9b79462f12f
jessica <ladyjtm@gmail.com>
2015-02-20 09:26:44 -0800

    finish  word class. Add instance and method of definition

--- lib/word.rb
+++ lib/word.rb
@@ -4,5 +4,37 @@ class Word
 
   def initialize(attributes)
     @vocabulary = attributes[:vocabulary]
+    @defintitions = []
+    @id = @@all_words.length + 1
+  end
 
+  def save
+    @@all_words.push(self)
+  end
+
+  def self.all
+    @@all_words
+  end
+
+  def self.clear
+    @@all_words = []
+  end
+
+  def id
+    @id
+  end
+
+  def add_word(new_word)
+    @definitions.push(new_word)
+  end
+
+  def self.find_word(identification)
+    found_word = nil
+    @@all_words.each do |word|
+      if word.id == identification
+        found_word = word
+      end
+    end
+    found_word
+  end
 end
